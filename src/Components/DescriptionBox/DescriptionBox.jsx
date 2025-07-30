1998995dee20e2f7bf884aa2b3d7224cb377d0b4
ElanurToptas <elanur.toptass@gmail.com>
2025-07-30 12:37:28 +0300

    descriptionBox add

--- src/Components/DescriptionBox/DescriptionBox.jsx
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -3,6 +3,29 @@ import "./DescriptionBox.scss"
 
 export const DescriptionBox = () => {
   return (
-    <div className='descriptionBox'>DescriptionBox</div>
+    <div className='descriptionBox'>
+        <div className="dascriptionBox-navigator">
+            <div className="descriptionBox-nav-box">
+                Description
+            </div>
+            <div className="descriptionBox-nav-box fade">
+                Reviews (122)
+            </div>
+        </div>
+        <div className="descriptionBox-description">
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Magni veritatis tenetur distinctio quos qui. Explicabo, deleniti!
+                Amet corporis ut a error cupiditate, accusamus sit harum provident earum.
+                Fuga, inventore unde!
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Distinctio, ut sint asperiores, recusandae incidunt velit
+                harum quibusdam laborum unde, sed voluptatum consectetur 
+                exercitationem eos deserunt? Porro blanditiis aliquam doloribus sed.
+            </p>
+        </div>
+    </div>
   )
 }
