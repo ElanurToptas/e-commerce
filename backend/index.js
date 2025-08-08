0f8d3e9dcffc4a94878241c5a4d9cad72b17972f
ElanurToptas <elanur.toptass@gmail.com>
2025-08-09 02:04:12 +0300

    Backend connection for products completed

--- backend/index.js
+++ backend/index.js
@@ -107,13 +107,50 @@ const Product = mongoose.model("Product",{
     }
 })
 
-addEventListener.post('/addproduct',async (req,res)=>{
+app.post('/addproduct',async (req,res)=>{
+    let Products = await Product.find({});
+    let id;
+    if(Products.length>0){
+        let last_product_array = Products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id = 1;
+    }
     const product= new Product({
         id: req.body.id,
         name: req.body.name,
-        iamge: req.body.image,
+        images: req.body.images,
         category: req.body.category,
         new_price:req.body.new_price,
         old_price:req.body.old_price,
     });
+    console.log(product);
+    await product.save();
+     console.log("Saved");
+     res.json({
+        success:true,
+        name:req.body.name,
+     })
+})
+
+
+// Creating API for deleting products
+app.post('/removeproduct', async(req,res) => {
+    await Product.findOneAndDelete({
+        id:req.body.id
+    });
+    console.log("removed");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+// Creating API for getting all products
+app.get('/allproducts', async(req,res)=>{
+    let products = await Product.find({});
+    console.log("all products fetched");
+    res.send(products);
 })
