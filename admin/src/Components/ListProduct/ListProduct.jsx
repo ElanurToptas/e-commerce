95772c6dc3fa51ecf38ba1c26732a3a7e3a1a8be
ElanurToptas <elanur.toptass@gmail.com>
2025-08-12 13:04:17 +0300

    ListProduct Component design completed

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./ListProduct.css";
 import cross_icon from '../../Assets/Admin_Assets/cross_icon.png'
 
 export const ListProduct = () => {
   const [allproducts, setAllproducts] = useState([]);
 
-  const fetchInfo = async () => {
+ const fetchInfo = async () => {
+  try {
     const { data } = await axios.get("http://localhost:4000/allproducts");
     setAllproducts(data);
-  };
+  } catch (err) {
+    console.error("Ürünleri çekerken hata:", err);
+  }
+};
 
   useEffect(() => {
     fetchInfo();
@@ -18,6 +23,7 @@ export const ListProduct = () => {
     <div className="list-product">
       <h1>All Products List</h1>
       <div className="listproduct-format-main">
+        <p>Product</p>
         <p>Title</p>
         <p>Old Price</p>
         <p>New Price</p>
@@ -27,7 +33,7 @@ export const ListProduct = () => {
       <div className="listproduct-allproducts">
         <hr />
         {allproducts.map((product,index) =>{
-          return <div key={index} className="listproduct-format-main listproduct-format">
+          return <><div key={index} className="listproduct-format-main listproduct-format">
             <img  className ="listproduct-product-icon" src={product.images} alt="" />
             <p>{product.name}</p>
             <p>${product.old_price}</p>
@@ -35,6 +41,8 @@ export const ListProduct = () => {
             <p>{product.category}</p>
             <img className="listproduct-remove-icon" src={cross_icon} alt="" />
           </div>
+          <hr />
+          </> 
         })}
       </div>
     </div>
