f53a4a240182daf688df3a73dd94eada87554e96
ElanurToptas <elanur.toptass@gmail.com>
2025-08-17 22:20:47 +0300

    pay component update

--- src/Components/Pay/pay.jsx
+++ src/Components/Pay/pay.jsx
@@ -1,10 +1,30 @@
-import React,{useState} from 'react'
+import React,{useState, useEffect} from 'react'
+import axios from "axios";
 import './pay.scss'
 import Modal from '../Adress/model/model';
 import { CartForm } from './CartForm/CartForm';
+import ziraat from '../Assets/ziraat.png';
 export const Pay = () => {
 
   const [showModal, setShowModal] = useState(false);
+  const [new_cart, setNew_cart] = useState(null);
+
+    useEffect(() => {
+    axios
+      .get("http://localhost:4000/paymentt", {
+        headers: {
+          "auth-token": localStorage.getItem("auth-token"),
+        },
+      })
+      .then((response) => {
+        const lastCart = response.data[response.data.length - 1];
+        console.log("Son adres:", lastCart);
+        setNew_cart(lastCart);
+      })
+      .catch((error) => {
+        console.error("Veriler alınırken hata oluştu:", error);
+      });
+  }, []);
 
   return (
     <div className='pay-container'>
@@ -16,15 +36,25 @@ export const Pay = () => {
           <h3>Payment Options</h3>
           <button onClick={() => setShowModal(true)}>+ Add Cart</button>
         </div>
-        <div className="add-cart-selector">
-          <p>Kayıtlı kart yok</p>
+        <div className="add-cart-selectors">
+          <img src={ziraat} alt="" />
+          <div className="add-cart-selector">
+            {new_cart ? (
+            <p>
+              {new_cart.cartNumber}-{new_cart.expiryDate}
+            </p>
+          ) : (
+            <p>Kayıtlı Kart bulunamadı</p>
+          )}
           <i className="fa-solid fa-arrow-right"></i>
+          </div>
         </div>
       </div>
         {showModal && (
         <Modal onClose={() => setShowModal(false)}>
           <h2>Add Cart</h2>
-         <CartForm />
+         <CartForm 
+         />
         </Modal>
       )}
     </div>
